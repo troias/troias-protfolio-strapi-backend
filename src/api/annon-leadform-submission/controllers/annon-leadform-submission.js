441d14f9c97d4ec4be8605a7f34e77ad6316d715
troy <hunter007nz@gmail.com>
2022-06-11 22:52:59 +1200

    added email for contact form + added codesandboxurl

--- src/api/annon-leadform-submission/controllers/annon-leadform-submission.js
+++ src/api/annon-leadform-submission/controllers/annon-leadform-submission.js
@@ -6,4 +6,27 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::annon-leadform-submission.annon-leadform-submission');
+module.exports = createCoreController('api::annon-leadform-submission.annon-leadform-submission', (ctx) => {
+
+    // const { data } = ctx.request.body;
+    // console.log("controllerData", data);
+    return {
+        async create(ctx) {
+         
+            const { data } = ctx.request.body;
+
+            console.log("CreateControllerData", data);
+
+            await strapi.plugins['email'].services.email.send({
+                to: 'troyflavel@gmail.com',
+                from: `${data.email_address}`,
+        
+                replyTo: 'troyflavel@gmail.com',
+                subject: `Portfolio page email from ${data.name}`,
+                text: `Troias!, ${data.name} has sent you a message from your portfolio page.\n\n${data.message}`,
+                html: `<p>Troias!, ${data.name} has sent you a message from your portfolio page.</p>\n<p>${data.message}</p>`
+              });
+
+        }
+    };
+});
