c2d5d52aea9c439d400663d7be7197d056ce23d8
troy <hunter007nz@gmail.com>
2022-06-12 19:31:44 +1200

    added keys

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,11 +1,9 @@
 module.exports = ({ env }) => ({
     proxy: true,
     url: env('MY_HEROKU_URL'),
-  
     admin: {
         auth: {
             secret: env('JWT_SECRET'),
-            token: env('JWT_SECRET'),
         }
     }, 
     app: {
